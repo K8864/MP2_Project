0f319c8a8ccc8e4299bed71245ae607f26810dbc
K8864 <122418663+K8864@users.noreply.github.com>
2023-01-11 15:24:47 -0500

    Update Entity.java

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -5,7 +5,6 @@ import java.awt.image.BufferedImage;
 
 public class Entity {
     public int worldX, worldY;
-    public int speed;
     public BufferedImage left, left2, down, down2, right, right2, up, up2;
     public String direction;
     public int spriteCounter = 0;
@@ -13,6 +12,8 @@ public class Entity {
     public Rectangle solidArea;
     public boolean collisionOn = false;
 
+    public int hp;
+    public int speed;
 
     public boolean onPath = false;
 }
